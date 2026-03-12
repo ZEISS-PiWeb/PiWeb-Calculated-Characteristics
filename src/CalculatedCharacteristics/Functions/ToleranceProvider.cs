0c79fcbbad5b320dcdb97b1274da8ba549946dfc
Ronald Heckel <ronald.heckel@zeiss.com>
2026-03-12 20:51:41 +0100

    [PIWEB-23709] fix: PT-PROFILE not depends on tolerances
    The original definition defines to calculate the distance between the maximum and minimum dimension. The dimension for a point is calculated by the distance between the measured value and the target/desired value. The originator states to use always "0" for the desired value. But we changed it to support using the desired value attribute if the profile is biased.

--- src/CalculatedCharacteristics/Functions/ToleranceProvider.cs
+++ src/CalculatedCharacteristics/Functions/ToleranceProvider.cs
@@ -12,8 +12,6 @@ namespace Zeiss.PiWeb.CalculatedCharacteristics.Functions
 {
 	#region usings
 
-	using System;
-	using System.Globalization;
 	using Zeiss.PiWeb.Api.Definitions;
 
 	#endregion
@@ -23,58 +21,19 @@ namespace Zeiss.PiWeb.CalculatedCharacteristics.Functions
 	/// </summary>
 	internal static class ToleranceProvider
 	{
-		#region members
-
-		/// <summary>
-		/// Delegate for retrieving attribute values.
-		/// </summary>
-		public delegate object? AttributeHandler( ushort key );
-
-		#endregion
-
 		#region methods
-
-		private static double? GetDoubleAttributeValue( AttributeHandler attributeHandler, ushort attributeKey )
-		{
-			var value = attributeHandler( attributeKey );
-			if( value == null )
-				return null;
-
-			switch( value )
-			{
-				case double doubleValue:
-					return doubleValue;
-
-				case string stringValue:
-					if( double.TryParse( stringValue, NumberStyles.Float, CultureInfo.InvariantCulture, out var parsedValue ) )
-						return parsedValue;
-
-					return null;
-
-				default:
-					try
-					{
-						// Needed to be able to return a double value for a boxed integer value
-						return Convert.ToDouble( value, CultureInfo.InvariantCulture );
-					}
-					catch
-					{
-						return null;
-					}
-			}
-		}
 
 		public static Tolerance GetTolerance( AttributeHandler attributeHandler )
 		{
-			var lower = GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.LowerSpecificationLimit );
-			var upper = GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.UpperSpecificationLimit );
+			var lower = AttributeReader.GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.LowerSpecificationLimit );
+			var upper = AttributeReader.GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.UpperSpecificationLimit );
 
 			if( !lower.HasValue && !upper.HasValue )
 			{
-				var nominal = GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.NominalValue );
+				var nominal = AttributeReader.GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.NominalValue );
 
-				lower = nominal + GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.LowerTolerance );
-				upper = nominal + GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.UpperTolerance );
+				lower = nominal + AttributeReader.GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.LowerTolerance );
+				upper = nominal + AttributeReader.GetDoubleAttributeValue( attributeHandler, WellKnownKeys.Characteristic.UpperTolerance );
 			}
 
 			return new Tolerance( lower, upper );
